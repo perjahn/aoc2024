bccf28710bb3328b98d24c4b516c5d89abbf438c
Per Jahn <perjahn@gmail.com>
2024-12-14 16:33:24 +0100

    Docker

---
+++ readme.md
@@ -0,0 +1,1 @@
+[![Build](https://github.com/perjahn/aoc2024/actions/workflows/build.yml/badge.svg)](https://github.com/perjahn/aoc2024/actions/workflows/build.yml)
--- runall.ps1
+++ runall.ps1
@@ -9,7 +9,17 @@ foreach ($problem in $problems) {
   pushd $name
 
   Write-Host "*** $name ***" -f Green
+
+  if (!(Test-Path "example.txt")) {
+    Write-Host "No example.txt file." -f Yellow
+    continue
+  }
+
   sed -i 's/input.txt/example.txt/g' Program.cs
+  if ($name -eq '141') {
+    sed -i 's/101/11/g' Program.cs
+    sed -i 's/103/7/g' Program.cs
+  }
   dotnet run
 
   popd
